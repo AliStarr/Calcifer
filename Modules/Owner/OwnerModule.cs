e09dfe9ff762088e4617ed54e265b2feafe4eb61
Alister Haseldine <alister.haseldine@gmail.com>
2023-08-11 22:50:56 +0930

    Rework OwnerModule for SlashCommands.

--- Modules/Owner/OwnerModule.cs
+++ Modules/Owner/OwnerModule.cs
@@ -1,113 +1,95 @@
-﻿//using Calcifer.Common;
-//using Calcifer.Preconditions;
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//using Discord;
-//using Discord.Commands;
-//using Discord.WebSocket;
+namespace Calcifer.Modules.Owner
+{
+    [RequireOwner()]
+    public class OwnerModule : InteractionModuleBase<SocketInteractionContext>
+    {
 
-//using System;
-//using System.IO;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+        [SlashCommand("stop", "Stops the bot from running.")]
+        public async Task Stop()
+        {
+            await RespondAsync("Stopping Calcifer.");
+            Console.WriteLine("Bot Commanded to Stop!");
+            Environment.Exit(1);
+        }
 
-//namespace Calcifer.Modules.Owner
-//{
-//    [MinPermissions(AccessLevel.BotOwner)]
-//    public class OwnerModule : ModuleBase<SocketCommandContext>
-//    {
-//        readonly Random rnd = new();
-//        int rndResult;
-
-//        [Command("Stop")]
-//        [Alias("Quit")]
-//        [Summary("Exits the bot from running - _Owner Only_")]
-//        [Remarks("~Stop")]
-//        [Priority(1)]
-//        public async Task Stop()
-//        {
-//            rndResult = rnd.Next(1, CommonStrings.quitStrings.Length);
-//            await ReplyAsync(CommonStrings.quitStrings[rndResult]);
-//            Environment.Exit(1);
-//        }
         
-//        [Command("Restart")]
-//        [Alias("Reboot")]
-//        [Summary("Restarts the Bot - _Owner Only_")]
-//        [Remarks("~Restart")]
-//        [RequireUserPermission(GuildPermission.Administrator)]
-//        [Priority(1)]
-//        public async Task Restart()
-//        {
-//            rndResult = rnd.Next(1, CommonStrings.restartStrings.Length);
-//            await ReplyAsync(CommonStrings.restartStrings[rndResult]);
-//            Environment.Exit(0);
-//        }
-        
-//        [Command("ServerList")]
-//        [Summary("Gets all the servers the bot is connected to.")]
-//        [Remarks("~Serverlist")]
-//        [Alias("sl")]
-//        public async Task ServerListAsync()
-//        {
-//            var client = Context.Client as DiscordSocketClient;
-//            var embed = new EmbedBuilder();
-//            foreach (SocketGuild guild in client.Guilds)
-//            {
-//                embed.AddField(x =>
-//                {
-//                    x.Name = $"{guild.Name} || {guild.Id}";
-//                    x.Value = $"Guild Owner: {guild.Owner} || {guild.OwnerId}\nGuild Members: {guild.MemberCount}";
-//                    x.IsInline = true;
-//                });
-//            }
-//            embed.Title = "=== Server List ===";
-//            embed.Color = new Color(255, 210, 50);
-//            embed.Footer = new EmbedFooterBuilder()
-//            {
-//                Text = $"Total Guilds: {client.Guilds.Count}"
-//            };
-//            await ReplyAsync("", embed: embed.Build());
-//        }
+        [SlashCommand("restart", "Restarts the bot.")]
+        public async Task Restart()
+        {
+            await RespondAsync("Restarting. Currently doesn't restart. Gotta rework it.");
+            Console.WriteLine("Bot Commanded to restart!");
+            Environment.Exit(0); // TODO: Actually restart the bot. 
+        }
 
-//        [Command("Leave")]
-//        [Summary("Forces the bot to leave the specified server")]
-//        [Remarks("~Leave <ServerID> <Message>")]
-//        public async Task LeaveAsync(ulong ServerId, [Remainder] string msg)
-//        {
-//            if (string.IsNullOrWhiteSpace(msg))
-//                await ReplyAsync("You must provide a reason for leaving the server.");
-//            var client = Context.Client;
-//            var gld = client.GetGuild(ServerId);
-//            var ch = gld.DefaultChannel;
-//            var embed = new EmbedBuilder()
-//            {
-//                Description = $"Calcifer has been forced to leave this Server by its owner.\n**Reason:** {msg}",
-//                Color = new Color(255, 0, 0),
-//                Author = new EmbedAuthorBuilder()
-//                {
-//                    Name = Context.User.Username,
-//                    IconUrl = Context.User.GetAvatarUrl()
-//                }
-//            };
-//            await ch.SendMessageAsync("", embed: embed.Build());
-//            await Task.Delay(5000);
-//            await gld.LeaveAsync();
-//            await ReplyAsync($"Calcifer has left {ServerId}.");
-//        }
+        [SlashCommand("serverlist", "Gets a list of all servers the bot is connected to")]
+        public async Task ServerList()
+        {
+            var client = Context.Client as DiscordSocketClient;
+            var embed = new EmbedBuilder();
+            foreach (SocketGuild guild in client.Guilds)
+            {
+                embed.AddField(x =>
+                {
+                    x.Name = $"{guild.Name} || {guild.Id}";
+                    x.Value = $"Guild Owner: {guild.Owner} || {guild.OwnerId}\nGuild Members: {guild.MemberCount}";
+                    x.IsInline = true;
+                });
+            }
+            embed.Title = "=== Server List ===";
+            embed.Color = new Color(255, 210, 50);
+            embed.Footer = new EmbedFooterBuilder()
+            {
+                Text = $"Total Guilds: {client.Guilds.Count}"
+            };
+            await RespondAsync("", embed: embed.Build());
+        }
 
-//        [Command("Broadcast")]
-//        [Summary("Sends a message to ALL severs that the bot is connected to.")]
-//        [Remarks("~Broadcast <Message>")]
-//        [Alias("Yell", "Shout")]
-//        public async Task AsyncBroadcast([Remainder] string msg)
-//        {
-//            var glds = (Context.Client as DiscordSocketClient).Guilds;
-//            var defaultchan = glds.Select(g => g.GetChannel(g.Id)).Cast<ITextChannel>();
-//            await Task.WhenAll(defaultchan.Select(c => c.SendMessageAsync(msg)));
-//        }
+        [SlashCommand("leave", "Leave the specified server with a message.")]
+        public async Task Leave(ulong ServerId, string msg)
+        {
+            if (string.IsNullOrWhiteSpace(msg))
+            {
+                await ReplyAsync("You must provide a reason for leaving the server.");
+            }
+                
+            var client = Context.Client;
+            var gld = client.GetGuild(ServerId);
+            var ch = gld.DefaultChannel;
+            var embed = new EmbedBuilder()
+            {
+                Description = $"Calcifer has been forced to leave this Server by its owner.\n**Reason:** {msg}",
+                Color = new Color(255, 0, 0),
+                Author = new EmbedAuthorBuilder()
+                {
+                    Name = Context.User.Username,
+                    IconUrl = Context.User.GetAvatarUrl()
+                }
+            };
+            await ch.SendMessageAsync("", embed: embed.Build());
+            await Task.Delay(5000);
+            await gld.LeaveAsync();
+            await RespondAsync($"Calcifer has left {ServerId}.");
+        }
 
-//        private static MemoryStream GenerateStreamFromString(string value) 
-//            => new(Encoding.Unicode.GetBytes(value ?? ""));
-//    }
-//}
+        [SlashCommand("broadcast", "Sends a message to the default channel of all connected servers.")]
+        public async Task AsyncBroadcast(string msg)
+        {
+            var glds = (Context.Client as DiscordSocketClient).Guilds;
+            var defaultchan = glds.Select(g => g.GetChannel(g.Id)).Cast<ITextChannel>();
+            await Task.WhenAll(defaultchan.Select(c => c.SendMessageAsync(msg)));
+        }
+
+        private static MemoryStream GenerateStreamFromString(string value)
+            => new(Encoding.Unicode.GetBytes(value ?? ""));
+    }
+}
